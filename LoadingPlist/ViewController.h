de1c3af924cc26b695e533ca58c4d80657db4b4e
rebornwhu <rebornwhu@gmail.com>
2015-05-03 09:08:12 -0700

    first commit

---
+++ LoadingPlist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadingPlist
+//
+//  Created by Xiao Lu on 5/2/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDataSource>
+
+
+@end
+
---
+++ LoadingPlist/ViewController.m
@@ -0,0 +1,79 @@
+//
+//  ViewController.m
+//  LoadingPlist
+//
+//  Created by Xiao Lu on 5/2/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController {
+    NSDictionary *courseDetails;
+    NSArray *justCourseNames;
+    
+    NSDictionary *webCourseDetails;
+    NSArray *webCourseNames;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return @"iOS Courses";
+    }
+    else {
+        return @"Web Courses";
+    }
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return courseDetails.count;
+    }
+    else {
+        return webCourseDetails.count;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    if (indexPath.section == 0) {
+        cell.textLabel.text = justCourseNames[indexPath.row];
+    }
+    else {
+        cell.textLabel.text = webCourseNames[indexPath.row];
+    }
+    
+    return cell;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"courses"
+                                         withExtension:@"plist"];
+    courseDetails = [NSDictionary dictionaryWithContentsOfURL:url];
+    justCourseNames = courseDetails.allKeys;
+    
+    
+    NSURL *urlWeb = [[NSBundle mainBundle] URLForResource:@"courses_web"
+                                         withExtension:@"plist"];
+    webCourseDetails = [NSDictionary dictionaryWithContentsOfURL:urlWeb];
+    webCourseNames = webCourseDetails.allKeys;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
